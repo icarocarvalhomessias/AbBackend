6e454f976bd54556e47aee698cb6459c36f8e756
Icaro Carvalho <icarocmessias@gmail.com>
2025-02-27 09:59:41 -0300

    SalesItem Entity/Validation and Unit test

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/SaleItem.cs
@@ -0,0 +1,58 @@
+﻿using Ambev.DeveloperEvaluation.Common.Validation;
+using Ambev.DeveloperEvaluation.Domain.Common;
+using Ambev.DeveloperEvaluation.Domain.Validation;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class SaleItem : BaseEntity
+    {
+        public Guid ProductId { get; set; }
+        public Guid SaleId { get; set; }
+        public int Quantity { get; set; } = 1;
+        public decimal UnitPrice { get; set; } = 0;
+        public decimal TotalAmount { get; set; } = 0;
+        public bool IsCancelled { get; set; } = false;
+
+        public Sale Sale { get; set; }
+        public Product Product { get; set; }
+
+        // EF Constructor
+        protected SaleItem() { }
+
+        public SaleItem(Product product, int quantity, Guid saleId)
+        {
+            ProductId = product.Id;
+            Quantity = quantity;
+            UnitPrice = product.Price;
+            SaleId = saleId;
+            CalculateTotalAmount();
+        }
+
+        public void CalculateTotalAmount()
+        {
+            TotalAmount = (UnitPrice * Quantity);
+        }
+
+        public void ChangeQuantity(int quantity)
+        {
+            Quantity = quantity;
+            CalculateTotalAmount();
+        }
+
+        public void Cancel()
+        {
+            IsCancelled = true;
+        }
+
+        public ValidationResultDetail Validate()
+        {
+            var validator = new SaleItemValidator();
+            var result = validator.Validate(this);
+            return new ValidationResultDetail
+            {
+                IsValid = result.IsValid,
+                Errors = result.Errors.Select(o => (ValidationErrorDetail)o)
+            };
+        }
+    }
+}
