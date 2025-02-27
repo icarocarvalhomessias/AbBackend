06ba2d316b8522cd3375aba223253358ef9806c9
Icaro Carvalho <icarocmessias@gmail.com>
2025-02-27 09:42:52 -0300

    Product Entity/Validation and Unit test.

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Product.cs
@@ -0,0 +1,55 @@
+﻿using Ambev.DeveloperEvaluation.Common.Validation;
+using Ambev.DeveloperEvaluation.Domain.Common;
+using Ambev.DeveloperEvaluation.Domain.Validation;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+
+    public class Product : BaseEntity
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        public Rating Rating { get; set; } = new Rating();
+        public bool IsAvailable { get; protected set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public Product()
+        {
+            CreatedAt = DateTime.Now;
+        }
+
+        public void Activate()
+        {
+            UpdatedAt = DateTime.Now;
+            IsAvailable = true;
+        }
+
+        public void Deactivate()
+        {
+            UpdatedAt = DateTime.Now;
+            IsAvailable = false;
+        }
+
+        public ValidationResultDetail Validate()
+        {
+            var validator = new ProductValidator();
+            var result = validator.Validate(this);
+            return new ValidationResultDetail
+            {
+                IsValid = result.IsValid,
+                Errors = result.Errors.Select(o => (ValidationErrorDetail)o)
+            };
+        }
+
+    }
+
+    public class Rating
+    {
+        public decimal Rate { get; set; }
+        public int Count { get; set; }
+    }
+}
